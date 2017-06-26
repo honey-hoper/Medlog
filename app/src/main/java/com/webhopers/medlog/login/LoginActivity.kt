3676adbf020476395382ea785a773efca28f1227
Honey <harsh.matharu21@gmail.com>
2017-06-26 16:47:43 +0530

    Made Login Activity layout

---
+++ app/src/main/java/com/webhopers/medlog/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.webhopers.medlog.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.webhopers.medlog.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
