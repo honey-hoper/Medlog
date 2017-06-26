9a95343ad0db0b8beddb5913aa9062ec8153fcc6
Honey <harsh.matharu21@gmail.com>
2017-06-26 15:57:41 +0530

    Made Register Activity layout

---
+++ app/src/main/java/com/webhopers/medlog/register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.webhopers.medlog.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webhopers.medlog.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
